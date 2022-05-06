4afe284c9aae99934d5325f0efd5c171ee4048ac
Aléf Garrido <75386215+alef-garrido@users.noreply.github.com>
2022-05-06 04:06:20 -0500

    Update queries.sql
    Added SQL as per project requirements to add 4 rows into animals table,

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+INSERT INTO animals VALUES (DEFAULT, 'Agumon', '2020-02-03', 0, '1', 10.23);
+INSERT INTO animals VALUES (DEFAULT, 'Gabumon', '2018-11-15', 1, '0', 8);
+INSERT INTO animals VALUES (DEFAULT, 'Pikachu', '2021-06-07', 0, '1', 15.04);
+INSERT INTO animals VALUES (DEFAULT, 'Devimon', '2017-05-12', 5, '1', 11);
