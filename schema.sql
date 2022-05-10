3bd53171c55b56e6c01a8d87e83e9adec933f343
alef-garrido <oa.g11@icloud.com>
2022-05-10 13:41:15 -0500

    Remove colum <species>

--- schema.sql
+++ schema.sql
@@ -11,13 +11,17 @@ species     VARCHAR(250),
 PRIMARY KEY(id)
 );
 
+--Create a table named owners 
 CREATE TABLE owners(
   id           SERIAL PRIMARY KEY,
   full_name    VARCHAR(250),
   age          INT 
 );
-
+--Create a table named species
 CREATE TABLE species(
 id      SERIAL PRIMARY KEY,
 name    VARCHAR(250)
 );
+
+--Remove column species
+ALTER TABLE animals DROP COLUMN species;
